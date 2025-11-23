4c2291445ea8a988a6605da51114b0757f594800
Tempura-Roll <castillo.rafael0411@gmail.com>
2025-11-22 19:27:39 -0500

    Merged CMovimientos branch into main branch by taking only the new files. CMakeLists.txt updated to include CMovimientos header and implementation files

---
+++ CMovimientos.cpp
@@ -0,0 +1,27 @@
+//
+// Created by Carlos on 11/22/2025.
+//
+
+#include "CMovimientos.h"
+
+
+bool CMovimientos::validarMovimiento(int tablero_size, const vector<int>& pos_inicial) {
+    if (coordenadas.size() != 2)
+        return false;
+    int fila = coordenadas[0];
+    int columna = coordenadas[1];
+
+    if (!(fila >= 0 && fila < tablero_size && columna >= 0 && columna < tablero_size))
+        return false;
+
+    if (!validarDistancia_manhattan(pos_inicial))
+        return false;
+    return true;
+
+
+}
+
+bool CMovimientos::validarDistancia_manhattan(const vector<int>& pos_inicial) {
+    int dist = abs(coordenadas[0] - pos_inicial[0]) +  abs(coordenadas[1] - pos_inicial[1]);
+    return dist <= 1;
+}
---
+++ CMovimientos.h
@@ -0,0 +1,27 @@
+//
+// Created by Carlos on 11/22/2025.
+//
+
+#ifndef DOTS_AND_BOXES_CMOVIMIENTOS_H
+#define DOTS_AND_BOXES_CMOVIMIENTOS_H
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+class CMovimientos {
+private:
+    vector<int> coordenadas;
+public:
+    CMovimientos(){}
+    CMovimientos(const vector<int>& pos_final):coordenadas(pos_final){}
+    vector<int>& getCoordenadas(){return coordenadas;}
+
+    bool validarMovimiento(int tablero_size, const vector<int>& pos_inicial);
+    bool validarDistancia_manhattan(const vector<int>& pos_inicial);
+
+
+};
+
+
+#endif //DOTS_AND_BOXES_CMOVIMIENTOS_H
