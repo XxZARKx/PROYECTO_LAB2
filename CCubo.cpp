3294baf5347ebb0df6e4dca88e4cf07def749512
Tempura-Roll <castillo.rafael0411@gmail.com>
2025-11-30 00:19:57 -0500

    Maydelith changed this, IDK what it does.

--- CCubo.cpp
+++ CCubo.cpp
@@ -72,7 +72,7 @@ void CCubo :: verificar( vector<vector<string>> &estado_linea,const string& letr
             }
         }
         if (movimientos(pos[0],pos[1]+2,estado_linea.size())) {
-            if (estado_linea[pos[0]][pos[1]+2]==" | ") {
+            if (estado_linea[pos[0]][pos[1]+2]=="  |") {
                 estado.push_back(pos[0]);
                 estado.push_back(pos[1]+2);
             }
@@ -91,64 +91,27 @@ void CCubo :: verificar( vector<vector<string>> &estado_linea,const string& letr
         estado.clear();
 
         if (movimientos(pos[0],pos[1]-2,estado_linea.size())) {
-            if (estado_linea[pos[0]][pos[1]-2]==" | ") {
+            if (estado_linea[pos[0]][pos[1]-2]=="  |") {
                 estado.push_back(pos[0]);
                 estado.push_back(pos[1]-2);
             }
-
-        }
-        if (estado.size()==12) {
-            estado_linea[pos[0]][pos[1]-1]=" " + letra + " ";
-        }
-
-    }
-    else {
-        if (movimientos(pos[0]-1,pos[1]+1,estado_linea.size())) {
-            if (estado_linea[pos[0]-1][pos[1]+1]==" | ") {
-                estado.push_back(pos[0]-1);
-                estado.push_back(pos[1]+1);
-            }
         }
         if (movimientos(pos[0]-1,pos[1]-1,estado_linea.size())) {
-            if (estado_linea[pos[0]-1][pos[1]-1]==" | ") {
+            if (estado_linea[pos[0]-1][pos[1]-1]=="---") {
                 estado.push_back(pos[0]-1);
                 estado.push_back(pos[1]-1);
             }
 
         }
-        if (movimientos(pos[0]-2,pos[1],estado_linea.size())) {
-            if (estado_linea[pos[0]-2][pos[1]]==" ― ") {
-                estado.push_back(pos[0]-2);
-                estado.push_back(pos[1]);
+        if (movimientos(pos[0]+1,pos[1]-1,estado_linea.size())) {
+            if (estado_linea[pos[0]+1][pos[1]-1]=="---") {
+                estado.push_back(pos[0]+1);
+                estado.push_back(pos[1]-1);
             }
 
         }
         if (estado.size()==6) {
-            estado_linea[pos[0]-1][pos[1]]=" " + letra + " ";
-        }
-
-        if (movimientos(pos[0]+1,pos[1]+1,estado_linea.size())) {
-            if (estado_linea[pos[0]+1][pos[1]+1]==" | ") {
-                estado.push_back(pos[0]+1);
-                estado.push_back(pos[1]+1);
-            }
-        }
-        if (movimientos(pos[0]+1,pos[1]-1,estado_linea.size())) {
-            if (estado_linea[pos[0]+1][pos[1]-1]==" | ") {
-                estado.push_back(pos[0]+1);
-                estado.push_back(pos[1]-1);
-            }
-
-        }
-        if (movimientos(pos[0]+2,pos[1],estado_linea.size())) {
-            if (estado_linea[pos[0]+2][pos[1]]==" ― ") {
-                estado.push_back(pos[0]+2);
-                estado.push_back(pos[1]);
-            }
-
-        }
-        if (estado.size()==12) {
-            estado_linea[pos[0]+1][pos[1]]=" " + letra + " ";
+            estado_linea[pos[0]][pos[1]-1]="  " + letra;
         }
 
     }
