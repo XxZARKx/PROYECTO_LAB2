ab382f02e5a2a77733aedb403e80ad61fd0e2468
Tempura-Roll <castillo.rafael0411@gmail.com>
2025-11-30 00:19:12 -0500

    Maydelith added this, IDK what it does.

--- CCubo.cpp
+++ CCubo.cpp
@@ -74,7 +74,7 @@ void CCubo :: verificar( vector<vector<string>> &estado_linea,const string& letr
         if (movimientos(pos[0],pos[1]+2,estado_linea.size())) {
             if (estado_linea[pos[0]][pos[1]+2]==" | ") {
                 estado.push_back(pos[0]);
-            estado.push_back(pos[1]+2);
+                estado.push_back(pos[1]+2);
             }
 
         }
