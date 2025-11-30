625976f24c6ad5e48345e9bf28d0be680d766be3
Tempura-Roll <castillo.rafael0411@gmail.com>
2025-11-30 00:18:37 -0500

    Maydelith added this, IDK what it does.

--- CCubo.cpp
+++ CCubo.cpp
@@ -14,6 +14,19 @@ bool CCubo::movimientos(int n1,int n2, size_t tamano) {
 void CCubo :: verificar( vector<vector<string>> &estado_linea,const string& letra)const {
     vector<int> estado;
     if (lado=="columna") {
+        if (movimientos(pos[0]-2,pos[1],estado_linea.size())) {
+            if (estado_linea[pos[0]-2][pos[1]]=="---") {
+                estado.push_back(pos[0]-2);
+                estado.push_back(pos[1]);
+            }
+        }
+        if (movimientos(pos[0]-1,pos[1]-1,estado_linea.size())) {
+            if (estado_linea[pos[0]-1][pos[1]-1]=="  |") {
+                estado.push_back(pos[0]-1);
+                estado.push_back(pos[1]-1);
+            }
+
+        }
         if (movimientos(pos[0]-1,pos[1]+1,estado_linea.size())) {
             if (estado_linea[pos[0]-1][pos[1]+1]==" ― ") {
                 estado.push_back(pos[0]-1);
